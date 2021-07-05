fb006a34a6f6cb7f9b36fd248c22be1b6536f9b9
Max <maxssilva@yahoo.com.br>
2021-07-05 17:19:20 -0300

    Insercao de interface AutorRepository e modificação de metodo de criacao em CrudAutoresController

--- src/main/java/br/com/deveficiente/APIcasadocodigodeveficiente/detalheLivro/Autor.java
+++ src/main/java/br/com/deveficiente/APIcasadocodigodeveficiente/detalheLivro/Autor.java
@@ -2,16 +2,27 @@ package br.com.deveficiente.APIcasadocodigodeveficiente.detalheLivro;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 
 import org.hibernate.validator.constraints.URL;
 
+
+@Entity
 public class Autor {
 
-		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;	
+	
 	private @NotBlank String nome;
+	
 	private @NotBlank @URL String linkGithub;
+	 
 	@PastOrPresent
 	private LocalDateTime createAt = LocalDateTime.now();
 
--- src/main/java/br/com/deveficiente/APIcasadocodigodeveficiente/detalheLivro/CrudAutoresController.java
+++ src/main/java/br/com/deveficiente/APIcasadocodigodeveficiente/detalheLivro/CrudAutoresController.java
@@ -2,12 +2,16 @@ package br.com.deveficiente.APIcasadocodigodeveficiente.detalheLivro;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CrudAutoresController {
+	
+	@Autowired
+	private AutorRepository autorRepository;
 	 
 	@PostMapping(value =  "/api/autor")
 	/*marcar como valid para validar as informações e trazer a exception de erro no corpo da 
@@ -15,7 +19,7 @@ public class CrudAutoresController {
 	 * o corpo da requisição venha no formato JSON, do contrario vem como form urlencoded*/
 	public void novo (@Valid @RequestBody NovoAutorForm form) {
 		 Autor novoAutor=form.novoAutor();
-		 System.out.println(novoAutor);
-	 }		
+		 autorRepository.save(novoAutor);
+	}		
  
 }
